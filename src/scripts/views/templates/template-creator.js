181e08a6ab684e90f93f66589216eb1b8693a8a0
Viola <viola.yosevi19@gmail.com>
2022-11-23 11:25:11 +0700

    Project di lanjutkan menambah routing data modul

---
+++ src/scripts/views/templates/template-creator.js
@@ -0,0 +1,14 @@
+import '../../data/dashboard.json';
+
+const DashboardListItem = (dashboard) => {
+        `<article class="module-item">
+        <div class="module-item-picture">
+            <img src="${dashboard.pictureId}" alt="Picture">
+        </div>
+        <div class="container">
+        <h2 class="title"><a href="#">${dashboard.title}</a></h2>
+        </div>
+    </article>`;
+}
+
+export { DashboardListItem } ;
